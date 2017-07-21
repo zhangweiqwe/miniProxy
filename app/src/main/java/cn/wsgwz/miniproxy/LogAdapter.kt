5ac3ad80d0409864d250d8116fbe78dacc334ca7
admin <857899299@qq.com>
2017-07-21 11:36:17 +0800

    firsInit

---
+++ app/src/main/java/cn/wsgwz/miniproxy/LogAdapter.kt
@@ -0,0 +1,42 @@
+package cn.wsgwz.miniproxy
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by admin on 2017/7/21 0021.n
+ */
+class LogAdapter(var list: ArrayList<String> ,context:Context) : RecyclerView.Adapter<LogAdapter.ViewHolder>() {
+
+    var tag:String = "LogAdapter";
+
+    var inflater:LayoutInflater;
+    init {
+        inflater = LayoutInflater.from(context)
+    }
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.title_TV?.setText(list?.elementAt(position));
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        return ViewHolder(inflater!!.inflate(R.layout.view_log_adapter_item,parent,false));
+    }
+
+    override fun getItemCount(): Int {
+        Log.d(tag,""+list?.size?:"0");
+        return list?.size?:0;
+    }
+
+    class ViewHolder: RecyclerView.ViewHolder {
+        var title_TV:TextView;
+        constructor( itemView:View) : super(itemView) {
+            title_TV = itemView.findViewById<TextView>(R.id.title_TV);
+        }
+
+    }
+}
---
+++ app/src/main/java/cn/wsgwz/miniproxy/MainActivity.kt
@@ -0,0 +1,48 @@
+package cn.wsgwz.miniproxy
+
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var loglist:ArrayList<String> = ArrayList<String>();
+    lateinit var logAdapter:LogAdapter;
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        package_uid_TV.text = getCurrentUid(this)?.toString();
+        switch_Bn.setOnClickListener{
+
+        }
+
+
+        loglist?.add("demo");
+        logAdapter = LogAdapter(loglist,this);
+        var layoutMannager: LinearLayoutManager = LinearLayoutManager(this);
+        log_RV.layoutManager = layoutMannager;
+        log_RV.adapter = logAdapter;
+
+
+
+        loglist?.add("demo3");
+        logAdapter?.notifyDataSetChanged();
+
+    }
+
+
+    //获取uid
+    fun getCurrentUid(context:Context):Int?{
+        var packageManager:PackageManager = context?.packageManager;
+        var applicationInfo:ApplicationInfo = packageManager.getApplicationInfo(context.packageName,PackageManager.GET_ACTIVITIES);
+        return applicationInfo?.uid?:null;
+    }
+}
